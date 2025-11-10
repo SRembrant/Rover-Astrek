899be6a6b1b6a4ef73035156316a26c44851638a
Joad-Gaviria <joadgaviria@unicauca.edu.co>
2025-11-10 18:09:41 -0500

    Funcion de la SD y arreglo de la task de transmision

--- CodigoAstrek1/Debug/Core/Src/Transmision/subdir.mk
+++ CodigoAstrek1/Debug/Core/Src/Transmision/subdir.mk
@@ -6,25 +6,28 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../Core/Src/Transmision/LoRa_RYLR998.c \
-../Core/Src/Transmision/Serial.c 
+../Core/Src/Transmision/Serial.c \
+../Core/Src/Transmision/datalogger.c 
 
 OBJS += \
 ./Core/Src/Transmision/LoRa_RYLR998.o \
-./Core/Src/Transmision/Serial.o 
+./Core/Src/Transmision/Serial.o \
+./Core/Src/Transmision/datalogger.o 
 
 C_DEPS += \
 ./Core/Src/Transmision/LoRa_RYLR998.d \
-./Core/Src/Transmision/Serial.d 
+./Core/Src/Transmision/Serial.d \
+./Core/Src/Transmision/datalogger.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Src/Transmision/%.o Core/Src/Transmision/%.su Core/Src/Transmision/%.cyclo: ../Core/Src/Transmision/%.c Core/Src/Transmision/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I"D:/EQUIPO ATREK/REPO ROVER ALTERNATIVE/Rover-Astrek/CodigoAstrek1/Core/Inc/Actuadores" -I"D:/EQUIPO ATREK/REPO ROVER ALTERNATIVE/Rover-Astrek/CodigoAstrek1/Core/Src/Actuadores" -I"D:/EQUIPO ATREK/REPO ROVER ALTERNATIVE/Rover-Astrek/CodigoAstrek1/Core/Inc/Sensores" -I"D:/EQUIPO ATREK/REPO ROVER ALTERNATIVE/Rover-Astrek/CodigoAstrek1/Core/Inc/Transmision" -I"D:/EQUIPO ATREK/REPO ROVER ALTERNATIVE/Rover-Astrek/CodigoAstrek1/Core/Src/Sensores" -I"D:/EQUIPO ATREK/REPO ROVER ALTERNATIVE/Rover-Astrek/CodigoAstrek1/Core/Src/Transmision" -I"D:/EQUIPO ATREK/REPO ROVER ALTERNATIVE/Rover-Astrek/CodigoAstrek1/Core/Inc/Nav" -I"D:/EQUIPO ATREK/REPO ROVER ALTERNATIVE/Rover-Astrek/CodigoAstrek1/Core/Src/Nav" -I"D:/EQUIPO ATREK/REPO ROVER ALTERNATIVE/Rover-Astrek/CodigoAstrek1/Core/Inc/Herramientas" -I"D:/EQUIPO ATREK/REPO ROVER ALTERNATIVE/Rover-Astrek/CodigoAstrek1/Core/Src/Herramientas" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I"C:/Users/joadj/OneDrive/Escritorio/Universidad/Proyecto Astrek/RepoAstrek/Rover-Astrek/CodigoAstrek1/Core/Inc/Actuadores" -I"C:/Users/joadj/OneDrive/Escritorio/Universidad/Proyecto Astrek/RepoAstrek/Rover-Astrek/CodigoAstrek1/Core/Src/Actuadores" -I"C:/Users/joadj/OneDrive/Escritorio/Universidad/Proyecto Astrek/RepoAstrek/Rover-Astrek/CodigoAstrek1/Core/Inc/Sensores" -I"C:/Users/joadj/OneDrive/Escritorio/Universidad/Proyecto Astrek/RepoAstrek/Rover-Astrek/CodigoAstrek1/Core/Inc/Transmision" -I"C:/Users/joadj/OneDrive/Escritorio/Universidad/Proyecto Astrek/RepoAstrek/Rover-Astrek/CodigoAstrek1/Core/Src/Sensores" -I"C:/Users/joadj/OneDrive/Escritorio/Universidad/Proyecto Astrek/RepoAstrek/Rover-Astrek/CodigoAstrek1/Core/Src/Transmision" -I"C:/Users/joadj/OneDrive/Escritorio/Universidad/Proyecto Astrek/RepoAstrek/Rover-Astrek/CodigoAstrek1/Core/Inc/Nav" -I"C:/Users/joadj/OneDrive/Escritorio/Universidad/Proyecto Astrek/RepoAstrek/Rover-Astrek/CodigoAstrek1/Core/Src/Nav" -I"C:/Users/joadj/OneDrive/Escritorio/Universidad/Proyecto Astrek/RepoAstrek/Rover-Astrek/CodigoAstrek1/Core/Inc/Herramientas" -I"C:/Users/joadj/OneDrive/Escritorio/Universidad/Proyecto Astrek/RepoAstrek/Rover-Astrek/CodigoAstrek1/Core/Src/Herramientas" -I../FATFS/Target -I../FATFS/App -I../Middlewares/Third_Party/FatFs/src -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Core-2f-Src-2f-Transmision
 
 clean-Core-2f-Src-2f-Transmision:
-	-$(RM) ./Core/Src/Transmision/LoRa_RYLR998.cyclo ./Core/Src/Transmision/LoRa_RYLR998.d ./Core/Src/Transmision/LoRa_RYLR998.o ./Core/Src/Transmision/LoRa_RYLR998.su ./Core/Src/Transmision/Serial.cyclo ./Core/Src/Transmision/Serial.d ./Core/Src/Transmision/Serial.o ./Core/Src/Transmision/Serial.su
+	-$(RM) ./Core/Src/Transmision/LoRa_RYLR998.cyclo ./Core/Src/Transmision/LoRa_RYLR998.d ./Core/Src/Transmision/LoRa_RYLR998.o ./Core/Src/Transmision/LoRa_RYLR998.su ./Core/Src/Transmision/Serial.cyclo ./Core/Src/Transmision/Serial.d ./Core/Src/Transmision/Serial.o ./Core/Src/Transmision/Serial.su ./Core/Src/Transmision/datalogger.cyclo ./Core/Src/Transmision/datalogger.d ./Core/Src/Transmision/datalogger.o ./Core/Src/Transmision/datalogger.su
 
 .PHONY: clean-Core-2f-Src-2f-Transmision
 
